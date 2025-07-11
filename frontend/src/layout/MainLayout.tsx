0b5bc3d731008b7de487edcfe9e2f5e8fdbe5b4c
Saurabh Pandey <ampsaurabh88877@gmail.com>
2025-07-11 23:53:27 +0530

    .

---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,35 @@
+
+import  {ResizablePanelGroup, ResizablePanel, ResizableHandle}  from '@/components/ui/resizable'
+import { Outlet } from 'react-router-dom'
+const MainLayout = () => {
+    const isMobile= false;
+  return (
+    <div className='h-screen bg-black text-white flex flex-col'>
+      <ResizablePanelGroup direction='horizontal' className='h-full flex-1 flex w-full overflow-hidden p-2'>
+        {/* left SideBar*/ }
+        <ResizablePanel defaultSize={20} minSize={isMobile ?0:10} maxSize={30}>
+            left SideBar
+
+        </ResizablePanel>
+        <ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+        {/* Main Content Area */ }
+        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+          <div className='h-full w-full overflow-hidden'>
+            <Outlet />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+
+        {/* Right SideBar */ }
+        <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30} collapsedSize={0}>
+            friends Activity
+
+          </ResizablePanel>
+          <ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+        
+      </ResizablePanelGroup>
+    </div>
+  )
+}
+
+export default MainLayout
